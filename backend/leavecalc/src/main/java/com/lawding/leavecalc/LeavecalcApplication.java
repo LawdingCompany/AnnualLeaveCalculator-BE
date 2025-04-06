9e1b06db793fe688acf144ee70f1094789b732ed
김재윤 <jaeyun1723@naver.com>
2025-04-07 03:53:09 +0900

    [BE] chore : 프로젝트 환경설정

---
+++ backend/leavecalc/src/main/java/com/lawding/leavecalc/LeavecalcApplication.java
@@ -0,0 +1,13 @@
+package com.lawding.leavecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavecalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavecalcApplication.class, args);
+	}
+
+}
---
+++ backend/leavecalc/src/test/java/com/lawding/leavecalc/LeavecalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawding.leavecalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavecalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
