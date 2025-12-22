2fc85e2e07be3122ba59dcd39a1547e05fe26d8b
김재윤 <jaeyun1723@naver.com>
2025-12-23 05:58:40 +0900

    fix : DictionaryResponse - categoryDto -> category로 네이밍 변경

--- backend/leavecalc/src/main/java/com/lawding/leavecalc/domain/dictionary/dto/response/DictionaryResponse.java
+++ backend/leavecalc/src/main/java/com/lawding/leavecalc/domain/dictionary/dto/response/DictionaryResponse.java
@@ -3,7 +3,7 @@ package com.lawding.leavecalc.domain.dictionary.dto.response;
 import com.lawding.leavecalc.domain.dictionary.category.dto.response.DictionaryCategoryResponse;
 import com.lawding.leavecalc.domain.dictionary.entity.Dictionary;
 
-public record DictionaryResponse(Long id, DictionaryCategoryResponse categoryDto, String question,
+public record DictionaryResponse(Long id, DictionaryCategoryResponse category, String question,
                                  String content) {
 
     public static DictionaryResponse from(Dictionary dictionary) {
