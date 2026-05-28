071b03475895a2a579e1a26076a5a78e9b456755
김재윤 <jaeyun1723@naver.com>
2026-05-28 23:19:38 +0900

    feat : Holiday API 기능 구현 - Service

--- backend/leavecalc/src/main/java/com/lawding/leavecalc/domain/holiday/service/HolidayService.java
+++ backend/leavecalc/src/main/java/com/lawding/leavecalc/domain/holiday/service/HolidayService.java
@@ -1,5 +1,9 @@
 package com.lawding.leavecalc.domain.holiday.service;
 
+import com.lawding.leavecalc.domain.holiday.dto.HolidayDto;
+import java.util.List;
+
 public interface HolidayService {
 
+    List<HolidayDto> getHolidays(int year);
 }
